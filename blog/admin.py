4b263f30e20f6bb44b32a57043e02d430f6d656d
Sajad <sajadhatamiw@gmail.com>
2026-06-02 22:38:20 +0330

    making Tag & Category & Post & Image models in the blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from multiprocessing.resource_tracker import register
 
+from django.contrib import admin
+from .models import Tag, Post, Category, Image
 # Register your models here.
+
+
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Image)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from projects.models import STATUS_CHOICES
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    def __str__(self): return self.tag_name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    def __str__(self): return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    cover_image = models.ImageField(
+        upload_to='posts/images/',
+        blank=True, null=True
+    )
+    short_description = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='posts/images/', blank=True)
+    caption = models.TextField(blank=True)
+    def __str__(self):
+        return self.caption or f"Image for {self.post.title}"
