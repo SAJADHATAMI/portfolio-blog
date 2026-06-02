7c61613664d6890c422b250e22231f66d9d2aff1
Sajad <sajadhatamiw@gmail.com>
2026-06-02 23:10:05 +0330

    making Technology & Education & Experience models in the resume app

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Technology, Education, Experience
 # Register your models here.
+admin.site.register(Technology)
+admin.site.register(Education)
+admin.site.register(Experience)
--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Education(models.Model):
+    degree_title = models.CharField(max_length=150)
+    field_of_study = models.CharField(max_length=150)
+    institution = models.CharField(max_length=150)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    technologies = models.ManyToManyField(Technology, blank=True)
+
+    def __str__(self):
+        return f"{self.degree_title} - {self.institution}"
+
+
+class Experience(models.Model):
+    job_title = models.CharField(max_length=150)
+    company = models.CharField(max_length=150)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    technologies = models.ManyToManyField(Technology, blank=True)
+
+    def __str__(self):
+        return f"{self.job_title} - {self.company}"
